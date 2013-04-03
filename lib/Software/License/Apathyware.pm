232d7a406b2e1dba2f1829af07e05ce2e7ef2789
mkandel <marc.kandel.junk@gmail.com>
2013-04-03 05:47:29 -0700

    hmm maybe not wip

--- lib/Software/License/Apathyware.pm
+++ lib/Software/License/Apathyware.pm
@@ -7,16 +7,16 @@ use base 'Software::License';
 
 sub name { 'Apathyware License v1' }
 sub url  { 'https://www.google.com/search?num=100&hl=en&safe=off&site=&source=hp&q=apathyware&oq=apathyware' }
-sub meta_name  { 'apathyware' }
-sub meta2_name { 'apathyware_1' }
+sub meta_name  { 'open_source' }
+sub meta2_name { 'open_source' }
 
 
 1;
 
 __DATA__
-__NOTICE__
-This software is copyright (c) {{$self->year}} by {{$self->holder}}.
 __LICENSE__
+This software is copyright (c) {{$self->year}} by {{$self->holder}}.
+
 This code is released as Apathyware:
 
 "The code doesn't care what you do with it, and neither do I."
